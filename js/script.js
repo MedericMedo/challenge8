75dede59544fc4111431b40d0953392f4fdf47ef
Médéric Voisin <mederic.medo@gmail.com>
2016-02-26 09:38:24 +0100

    commit pour papa Alban !

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$().ready(function(){
+
+
+  Morris.Area({
+  element: 'area-example',
+  data: [
+    { y: '2006', a: 100, b: 90 },
+    { y: '2007', a: 75,  b: 65 },
+    { y: '2008', a: 50,  b: 40 },
+    { y: '2009', a: 75,  b: 65 },
+    { y: '2010', a: 50,  b: 40 },
+    { y: '2011', a: 75,  b: 65 },
+    { y: '2012', a: 100, b: 90 }
+  ],
+  xkey: 'y',
+  ykeys: ['a', 'b'],
+  labels: ['Series A', 'Series B'],
+  lineColors: ['red', 'grey']
+  });
+
+  Morris.Donut({
+  element: 'donut-example',
+  data: [
+    {label: "Download Sales", value: 12},
+    {label: "In-Store Sales", value: 30},
+    {label: "Mail-Order Sales", value: 20}
+  ]
+});
+
+
+
+
+});
